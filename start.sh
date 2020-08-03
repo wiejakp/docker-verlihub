2f743d39066264067a7d40141046e9516948f227
Your Name <you@example.com>
2020-08-03 07:12:47 +0000

    wip

--- start.sh
+++ start.sh
@@ -2,17 +2,43 @@
 
 option=${1:---install}
 
+log_dir="/etc/verlihub/logs"
+log_tls="${log_dir}/tls.log"
+log_hub="${log_dir}/hub.log"
+
 if [ ${option} = "--install" ]
 then
     echo ""
+    echo "starting installation"
+    sleep 5
+    echo ""
 
     address_local=$(ifconfig lo | awk '/inet / {print $2}');
-    address_public=$(ifconfig ens160 | awk '/inet / {print $2}');
+    address_public=$(ifconfig eth0 | awk '/inet / {print $2}');
 
     echo "Local Address: $address_local"
     echo "Public Address: $address_public"
     echo ""
 
+    echo "checking log directory"
+    if [ ! -d ${log_dir} ]; then
+        echo "creating log directory"
+        mkdir -p "${log_dir}"
+    fi
+
+    echo "checking tls log file"
+    if [[ ! -e "${log_tls}" ]]; then
+        echo "creating tls log file: ${log_tls}"
+        touch "${log_tls}"
+    fi
+
+    echo "checking hub log file"
+    if [[ ! -e "${log_hub}" ]]; then
+        echo "creating hub log file: ${log_hub}"
+        touch "${log_hub}"
+    fi
+
+    echo ""
     echo "Create SSH keys for TLS Proxy:"
     /usr/bin/openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out "/etc/verlihub/verlihub.crt" -keyout "/etc/verlihub/verlihub.key"
     echo ""
@@ -26,6 +52,11 @@ then
 
 elif [ ${option} = "--test" ]
 then
+    echo ""
+    echo "performing tests"
+    sleep 5
+    echo ""
+
     echo "Information of your container's network interfaces:"
     ifconfig
     echo ""
@@ -38,6 +69,7 @@ then
     read -p "enter mysql host: " mysqlHost
     read -p "enter mysql user: " mysqlUser
     read -s -p "enter mysql password: " mysqlPassword
+    echo ""
     echo ""
 
     if mysql -u$mysqlUser -p$mysqlPassword -h$mysqlHost -e ';'
@@ -53,25 +85,55 @@ then
 
 elif [ ${option} = "--run" ]
 then
-    address_local=$(ifconfig lo | awk '/inet / {print $2}');
-    address_public=$(ifconfig ens160 | awk '/inet / {print $2}');
+    echo ""
+    echo "starting tls"
+    sleep 5
 
+    address_local=$(ifconfig lo | awk '/inet / {print $2}');
+    address_public=$(ifconfig eth0 | awk '/inet / {print $2}');
+
+    echo "checking log directory"
+    if [ ! -d ${log_dir} ]; then
+        echo "creating log directory"
+        mkdir -p "${log_dir}"
+    fi
+
+    echo "checking tls log file"
+    if [[ ! -e "${log_tls}" ]]; then
+        echo "creating tls log file: ${log_tls}"
+        touch "${log_tls}"
+    fi
+
+    echo "checking hub log file"
+    if [[ ! -e "${log_hub}" ]]; then
+        echo "creating hub log file: ${log_hub}"
+        touch "${log_hub}"
+    fi
+
+    echo ""
     echo "Local Address: $address_local"
     echo "Public Address: $address_public"
     echo ""
 
-    echo sleep 10 seconds
-    sleep 10
-    /usr/src/verlihub-proxy/proxy --cert="/etc/verlihub/verlihub.crt" --key="/etc/verlihub/verlihub.key" --host="${address_public}:4111" --hub="${address_local}:4111"
+    #/usr/bin/nohup /usr/src/verlihub-proxy/proxy --cert="/etc/verlihub/verlihub.crt" --key="/etc/verlihub/verlihub.key" --host="${address_public}:4111" --hub="${address_local}:4111" &> "${log_tls}" &
+    /usr/bin/nohup /usr/src/verlihub-proxy/proxy --cert="/etc/verlihub/verlihub.crt" --key="/etc/verlihub/verlihub.key" --host="172.17.0.1:4111" --hub="${address_local}:4111" &> "${log_tls}" &
+
+    echo ""
+    echo "LOG TLS: ${log_tls}"
+    echo ""
+    echo "Completed!"
     echo ""
 
-    echo sleep 10 seconds
-    sleep 10
+    echo "starting hub"
+    sleep 5
     echo ""
 
     TERM=xterm
-    /usr/local/bin/vh_daemon /usr/local/bin/verlihub
+    /usr/bin/nohup /usr/local/bin/vh_daemon /usr/local/bin/verlihub &> "${log_hub}"
 
+    echo ""
+    echo "LOG HUB: ${log_hub}"
+    echo ""
     echo "Completed!"
     echo ""
 fi
